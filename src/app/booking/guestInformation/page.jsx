5c1d9bfbf7cc1bced2fbbb21d3b21ba4e0e3ed8b
Lochmann20 <Linemoller1907@gmail.com>
2024-06-04 19:48:47 +0200

    oversigt

--- src/app/booking/guestInformation/page.jsx
+++ src/app/booking/guestInformation/page.jsx
@@ -17,13 +17,28 @@ export default function Formpage() {
         <h1 className={`text-White text-3xl md:text-4xl mb-4 sm:mb-6 md:mb-8  ${BowlbyOne.className}`}>
           INFORMATION
         </h1>
+
+        {/* ordre er skjult på desktop men vist på mobil */}
+        <details className="md:hidden">
+              <summary className="mb-5">
+                <span>Oversigt over ordre</span>
+              </summary>
+          <div className="w-full md:flex">
+            <div className="w-full max-w-md mb-6">
+              <InformationBasket/>
+            </div>
+          </div>
+          </details>
+
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-10 xl:gap-12">
           <div className="w-full">
             <div className="w-full mb-6 md:mb-4 lg:mb-2">
               <GuestForm />
             </div>
           </div>
-          <div className="w-full md:flex">
+
+          {/* Ordre bliver skjult på mobil men vist på de andre */}
+          <div className="hidden md:hidden-none w-full md:flex">
             <div className="w-full max-w-md mb-6">
               <InformationBasket />
             </div>
