ca7f4e7e46a78ce9095a828606b1f4c39b4e48a7
Lochmann20 <Linemoller1907@gmail.com>
2024-06-13 18:13:21 +0200

    juster timer

--- src/app/components/Countdown/timer.jsx
+++ src/app/components/Countdown/timer.jsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 const CountdownTimer = ({ initialSeconds }) => {
   const [seconds, setSeconds] = useState(initialSeconds);
   const [showModal, setShowModal] = useState(false);
+  const [countdownActive, setCountdownActive] = useState(true);
 
   useEffect(() => {
     const storedSeconds = parseInt(sessionStorage.getItem('countdownSeconds')) || initialSeconds;
@@ -15,17 +16,24 @@ const CountdownTimer = ({ initialSeconds }) => {
         setSeconds((prevSeconds) => {
           const newSeconds = prevSeconds - 1;
           sessionStorage.setItem('countdownSeconds', newSeconds);
-          return newSeconds;
+          if (newSeconds <= 0) {
+            clearInterval(interval);
+            handleTimeout();
+          }
+          return newSeconds >= 0 ? newSeconds : 0; // Ensure seconds never go below 0
         });
       }, 1000);
       return () => clearInterval(interval);
     } else {
       handleTimeout();
     }
-  }, []);
+  }, [initialSeconds]);
 
   const handleTimeout = () => {
     setShowModal(true);
+    setSeconds(0); // Ensure seconds state is set to 0 when timeout occurs
+    setCountdownActive(false); // Indicate that countdown is no longer active
+    sessionStorage.removeItem('countdownSeconds'); // Clear sessionStorage
   };
 
   const handleCloseModal = () => {
@@ -33,10 +41,10 @@ const CountdownTimer = ({ initialSeconds }) => {
   };
 
   const handleLinkClick = () => {
-    // Reset sessionStorage and timer
-    sessionStorage.removeItem('countdownSeconds');
-    setSeconds(initialSeconds);
+    sessionStorage.removeItem('countdownSeconds'); // Clear sessionStorage
+    setSeconds(initialSeconds); // Reset seconds state to initial value
     setShowModal(false);
+    setCountdownActive(true); // Reset countdown active status
   };
 
   const formatTime = (seconds) => {
@@ -47,14 +55,18 @@ const CountdownTimer = ({ initialSeconds }) => {
 
   return (
     <div className="countdown-timer">
-      <p>Time remaining: {formatTime(seconds)}</p>
+      {countdownActive ? (
+        <p>Time remaining: {formatTime(seconds)}</p>
+      ) : (
+        <p>Time's up!</p>
+      )}
 
       <Modal
         title="Time's Up!"
         visible={showModal}
         footer={[
           <Link key="back-to-booking" href="/booking" onClick={handleLinkClick} passHref>
-           Go back to Booking
+            Go back to Booking
           </Link>,
         ]}
         onCancel={handleCloseModal}
