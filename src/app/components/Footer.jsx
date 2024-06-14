8eebb5c18932f680b933e4415fb5c4a2b0c17ae8
Lochmann20 <Linemoller1907@gmail.com>
2024-06-14 13:22:29 +0200

    newsletter-adjustments

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -12,33 +12,34 @@ const BowlbyOne = Bowlby_One({
 export default function Footer() {
   return (
     <section className='p-5 md:p-10 bg-Black'>
+      <div className='flex justify-center gap-28'>
 
-      <div>
-      <Nyhedsbrev/>
-      </div>
-      <div className='max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8  text-center md:text-left'>
+         <div className=''>
+          <h2 className={`text-White font-bold text-xl xl:text-1xl ${BowlbyOne.className}`}><span className='text-Hotpink'>© </span>FOO FESTIVAL</h2>
+          <p className='text-base text-White'>KEA - MMD 3. SEM</p>
+        </div>
         
-        <div className='order-2 md:order-1'>
-          <h2 className='text-Hotpink font-bold text-xl xl:text-1xl mb-2'>SUPPORT</h2>
+        <div className=''>
+          <h2 className='text-White font-bold text-xl xl:text-1xl'>SUPPORT</h2>
           <Link className='text-md md:text-base text-White hover:text-Hotpink' href="/about">About Foo Festival</Link> <br />
           <Link className='text-md md:text-base text-White hover:text-Hotpink' href="/privacy-policy">Privacy policy</Link> <br />
           <Link className='text-md md:text-base text-White hover:text-Hotpink' href="/termsofuse">Terms of use</Link>
           {/* <p className='text-md md:text-base text-White hover:text-Hotpink'>FAQ</p> */}
         </div>
 
-        <div className='order-1 md:order-2 md:flex md:flex-col md:items-center'>
-          <h2 className={`text-White font-bold text-xl xl:text-1xl ${BowlbyOne.className}`}><span className='text-Hotpink'>© </span>FOO FESTIVAL</h2>
-          <p className='text-base text-White'>KEA - MMD 3. SEM</p>
-        </div>
-
-        <div className='order-3 md:order-3 md:ml-0 lg:ml-32'>
-          <h2 className='text-Hotpink font-bold text-xl xl:text-1xl mb-2'>INFORMATION</h2>
+        <div className=''>
+          <h2 className='text-White font-bold text-xl xl:text-1xl'>INFORMATION</h2>
           <p className='text-md md:text-base text-White'>info@foofestival.com</p>
           <p className='text-md md:text-base text-White'>+45 12 34 54 32</p>
           <p className='text-md md:text-base text-White'>Festvej 1, 8000 Aarhus</p>
         </div>
 
+        <div>
+          <Nyhedsbrev/>
+        </div>
       </div>
     </section>
   );
 }
+
+// max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-8  text-center md:text-left
--- src/app/components/Nyhedsbrev.jsx
+++ src/app/components/Nyhedsbrev.jsx
@@ -4,9 +4,9 @@ function Nyhedsbrev() {
 
   return (
     <div className=''>
-        <div className='max-w-4xl mx-auto mb-20'>
-            <p className='text-Hotpink text-3xl sm:text-4xl mb-5 font-semibold'>Join our newsletter</p>
-            <p className='text-White max-w-xl text-lg sm:text-sm font-normal mb-5'>Dont miss out on exclusive Foo Festival updates! Sign up for our newsletter and be among the first to receive news about the line-up, ticket sales, and other exciting things</p>
+        <div className=''>
+            <p className='text-White text-xl xl:text-1xl font-bold uppercase'>Join our newsletter</p>
+            <p className='text-White max-w-md text-lg sm:text-sm font-normal mb-5'>Dont miss out on exclusive Foo Festival updates! Sign up for our newsletter and be among the first to receive news about the line-up, ticket sales, and other exciting things</p>
             
             <input 
             type="email" 
