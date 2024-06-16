78c8849980ac0e7e4d4584a6ed2aa79449d35cfd
Lochmann20 <Linemoller1907@gmail.com>
2024-06-16 09:58:18 +0200

    small things

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -6,12 +6,17 @@ import React from 'react';
 import FooTicket from '../components/FooTicket';
 import VipTicket from '../components/VipTicket';
 
+
+
 // set up font
 const BowlbyOne = Bowlby_One({
   subsets: ["latin"],
   weight: "400",
   display: "swap",
 });
+
+
+
 
 // main ticket component 
 export default function TicketsPage() {
